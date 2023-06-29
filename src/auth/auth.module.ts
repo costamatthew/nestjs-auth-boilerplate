5927faffebd9cf27a79e0f743efab0af36f6cb11
Mateus Costa <mateusdsilvacosta@outlook.com>
2023-06-28 23:34:44 -0300

    add user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
